538a40c7e43983d11720613650b9393e9e8b9abc
d116626 <d116626@gmail.com>
2023-01-12 11:55:29 -0300

    chore: rename some columns

---
+++ models/dashboard_construcoes_irregulares/selecao_monitor_verde.sql
@@ -0,0 +1,12 @@
+
+SELECT
+    id_global, 
+    id_boletim, 
+    periodo_boletim, 
+    acesso_local, 
+    tipo_local, 
+    observacao, 
+    nome_unidade_conservacao, 
+    area, 
+    geometry,
+FROM `rj-seop.conservacao_ambiental_monitor_verde.alertas_desmatamento`
