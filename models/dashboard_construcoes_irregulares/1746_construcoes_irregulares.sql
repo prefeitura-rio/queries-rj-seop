837119fc6a7cbdcb44edbbf69f86de06b93e34e4
d116626 <d116626@gmail.com>
2023-01-12 11:51:32 -0300

    chore: rename some columns

--- models/dashboard_construcoes_irregulares/1746_construcoes_irregulares.sql
+++ models/dashboard_construcoes_irregulares/1746_construcoes_irregulares.sql
@@ -7,8 +7,8 @@ SELECT
 	numero_logradouro, 
 	longitude, 
 	latitude,
-	nome_unidade_organizacional AS nome_unidade_org, 
-	id_unidade_organizacional_mae AS id_unidade_org_mae,
+	nome_unidade_organizacional, 
+	id_unidade_organizacional_mae,
 	tipo, 
 	subtipo, 
 	status, 
@@ -16,25 +16,21 @@ SELECT
 	tipo_situacao, 
 	dentro_prazo,
 	geometry 
-
 FROM `datario.administracao_servicos_publicos.chamado_1746`
 WHERE subtipo IN (	
-  
-   'Demolição'
-	,'Fiscalização de obras irregulares em comunidade - Áreas de Especial Interesse Social'
-	,'Fiscalização de obras em terreno invadido em Áreas de Especial Interesse Social'
-	,'Fiscalização de obras em andamento em Áreas Especiais'
-	,'Fiscalização de obras em comunidade'
-	,'Fiscalização de obras em comunidades'
-	,'Fiscalização de obras sem licença de expansão de comunidade'
-	
-	,'Fiscalização de construção irregular em áreas ambientalmente protegidas'
-	,'Fiscalização de ocupação por atividade irregular na natureza'
-	
-	,'Vistoria em construção irregular'
-  ,'Vistoria em invasão'
-
-  )
-
-AND categoria IN ('Serviço')
-AND numero_logradouro IS NOT NULL
+		'Demolição'
+		,'Fiscalização de obras irregulares em comunidade - Áreas de Especial Interesse Social'
+		,'Fiscalização de obras em terreno invadido em Áreas de Especial Interesse Social'
+		,'Fiscalização de obras em andamento em Áreas Especiais'
+		,'Fiscalização de obras em comunidade'
+		,'Fiscalização de obras em comunidades'
+		,'Fiscalização de obras sem licença de expansão de comunidade'
+		
+		,'Fiscalização de construção irregular em áreas ambientalmente protegidas'
+		,'Fiscalização de ocupação por atividade irregular na natureza'
+		
+		,'Vistoria em construção irregular'
+		,'Vistoria em invasão'
+	)
+	AND categoria IN ('Serviço')
+	AND numero_logradouro IS NOT NULL
