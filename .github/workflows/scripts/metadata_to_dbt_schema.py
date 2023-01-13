fdd7d27077f7467868172acc3954f7116c2e2b37
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-01-13 16:16:14 -0300

    fix(ci): metadata to dbt schema

--- .github/workflows/scripts/metadata_to_dbt_schema.py
+++ .github/workflows/scripts/metadata_to_dbt_schema.py
@@ -23,7 +23,6 @@ def dump_metadata_into_schema_yaml(
         if Path(schema_yaml_path).exists()
         else {"version": 2, "models": []}
     )
-
     if schema is None:
         schema = {"version": 2, "models": []}
 
@@ -42,7 +41,6 @@ def dump_metadata_into_schema_yaml(
 def format_table_description(table_info: dict) -> str:
     """
     Formats the table description.
-
     Input format:
     {
         "title": "some-title-here",
@@ -65,7 +63,6 @@ def format_table_description(table_info: dict) -> str:
             ...
         ]
     }
-
     Output format:
     **Descrição**: some long description here
     **Frequência de atualização**: some update frequency here
@@ -135,12 +132,17 @@ if __name__ == "__main__":
         # Iterate over tables
         for table_id in metadata[dataset_id]:
 
-            print(f"  - Table {table_id}")
+            if metadata[dataset_id][table_id].get("columns") is not None:
+                print(f"  - Table {table_id}")
 
-            table_metadata = {
-                "name": table_id,
-                "description": format_table_description(metadata[dataset_id][table_id]),
-                "columns": metadata[dataset_id][table_id]["columns"],
-            }
-
-            dump_metadata_into_schema_yaml(dataset_id, table_id, table_metadata)
+                table_metadata = {
+                    "name": table_id,
+                    "description": format_table_description(
+                        metadata[dataset_id][table_id]
+                    ),
+                    "columns": metadata[dataset_id][table_id]["columns"],
+                }
+                dump_metadata_into_schema_yaml(
+                    dataset_id, table_id, table_metadata)
+            else:
+                print(f"No columns for {table_id} in dataset {dataset_id}")
