1e11355c704c8c0974db7e39e742000d48c45a4d
d116626 <d116626@gmail.com>
2023-02-15 16:44:27 -0300

    chore: 1746 new query

--- models/dashboard_construcoes_irregulares/selecao_1746.sql
+++ models/dashboard_construcoes_irregulares/selecao_1746.sql
@@ -1,35 +1,34 @@
-
 SELECT 
-	id_chamado, 
-	data_inicio as data_inici, 
-	data_fim, 
-	id_logradouro as logradouro, 
-	numero_logradouro as numero_porta, 
+  id_chamado, 
+  REPLACE(SAFE_CAST(data_inicio AS STRING),'T',' ') data_inicio,
+  REPLACE(SAFE_CAST(data_fim AS STRING),'T',' ') data_fim,
+  id_logradouro, 
+  numero_logradouro, 
 
-	nome_unidade_organizacional as orgao, 
-	id_unidade_organizacional_mae as orgao_mae,
-	tipo, 
-	subtipo, 
-	status, 
-	situacao, 
-	tipo_situacao as tipo_situa, 
-	dentro_prazo as dentro_pra,
-	geometry 
+  nome_unidade_organizacional, 
+  id_unidade_organizacional_mae,
+  tipo, 
+  subtipo, 
+  status, 
+  situacao, 
+  tipo_situacao, 
+  dentro_prazo,
+
+  ST_GEOGPOINT(longitude, latitude) AS geometry 
 FROM `datario.administracao_servicos_publicos.chamado_1746`
 WHERE subtipo IN (	
-		'Demolição'
-		,'Fiscalização de obras irregulares em comunidade - Áreas de Especial Interesse Social'
-		,'Fiscalização de obras em terreno invadido em Áreas de Especial Interesse Social'
-		,'Fiscalização de obras em andamento em Áreas Especiais'
-		,'Fiscalização de obras em comunidade'
-		,'Fiscalização de obras em comunidades'
-		,'Fiscalização de obras sem licença de expansão de comunidade'
-		
-		,'Fiscalização de construção irregular em áreas ambientalmente protegidas'
-		,'Fiscalização de ocupação por atividade irregular na natureza'
-		
-		,'Vistoria em construção irregular'
-		,'Vistoria em invasão'
-	)
-	AND categoria IN ('Serviço')
-	AND numero_logradouro IS NOT NULL
+   'Demolição'
+	,'Fiscalização de obras irregulares em comunidade - Áreas de Especial Interesse Social'
+	,'Fiscalização de obras em terreno invadido em Áreas de Especial Interesse Social'
+	,'Fiscalização de obras em andamento em Áreas Especiais'
+	,'Fiscalização de obras em comunidade'
+	,'Fiscalização de obras em comunidades'
+	,'Fiscalização de obras sem licença de expansão de comunidade'
+	,'Fiscalização de construção irregular em áreas ambientalmente protegidas'
+	,'Fiscalização de ocupação por atividade irregular na natureza'
+	,'Vistoria em construção irregular'
+  ,'Vistoria em invasão'
+  )
+
+  AND categoria IN ('Serviço')
+  AND numero_logradouro IS NOT NULL;
